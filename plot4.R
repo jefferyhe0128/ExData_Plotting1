a20aabc505a1838c2975f8ee3abcaf41024aeaa7
Jeffery He <jefferyhe.math@gmail.com>
2016-05-29 17:35:15 +0100

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,8 +1,5 @@
-# Set as working directory
-setwd("~/Dropbox/Coursera/Data Science/04 Exploratory Data Analysis/Assignments/Week 1/Assignment_Course Project 1")
-
 # Load and subset the dataset
-data_full <- read.csv("household_power_consumption.txt", header = TRUE, sep = ';', na.strings = "?", 
+data_full <- read.csv("./data/household_power_consumption.txt", header = TRUE, sep = ';', na.strings = "?", 
                       nrows = 2075259, check.names = FALSE, stringsAsFactors = FALSE, comment.char = "", quote = '\"')
 data_full$Date <- as.Date(data_full$Date, format = "%d/%m/%Y")
 data <- subset(data_full, subset = (Date >= "2007-02-01" & Date <= "2007-02-02"))
